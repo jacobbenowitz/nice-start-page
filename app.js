c3328f8dfa5d4e45c497babb9a99fc08aade6d6f
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-05-16 13:24:26 -0400

    connected to server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
+// const users = require("./routes/api/users");
+// const links = require("./routes/api/links");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -15,14 +15,14 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!!"));
 
-app.use(passport.initialize());
-require('./config/passport')(passport);
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+// app.use("/api/users", users);
+// app.use("/api/links", links);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
