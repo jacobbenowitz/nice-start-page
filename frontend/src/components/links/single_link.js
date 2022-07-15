0767cf223d5f02c6607efc54ffc8dc674afc1eb0
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-07-15 00:25:15 -0400

    add gmeta to extract icon/image from urls

--- frontend/src/components/links/single_link.js
+++ frontend/src/components/links/single_link.js
@@ -6,10 +6,23 @@ export default class SingleLink extends React.Component {
   }
 
   render() {
-    const { title, url, section, date } = this.props.link;
+    const { title, url, section, date, metaData } = this.props.link;
+    let image, icon;
+
+    if ('image' in metaData) {
+      image = <img src={metaData.image} alt={title} />;
+    } else if ('og:image' in metaData) {
+      image = <img src={metaData['og:image']} alt={title}
+        style={{ width: '32px' }}
+      />;
+    }
+    if ('icon' in metaData) {
+      icon = <img src={metaData.icon} alt={title} />;
+    }
 
     return (
       <div className="link wrapper">
+        {icon ? icon : image ? image : null}
         <span className='link title'>{title}</span>
         <span className='link section'>{section}</span>
         <span className='link url'>{url}</span>
