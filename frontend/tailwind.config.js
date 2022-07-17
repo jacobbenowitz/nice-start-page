ad70d6048cc42f4e0c28f61d8134c811bcd99a53
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-07-16 21:40:05 -0400

    resolve dependencies and env errors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,38 +1,18 @@
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        background: colors.slate[900],
-        text: colors.slate[50],
-        surfaceDark: colors.slate[800],
-        surfaceLight: colors.slate[600],
-        lightGray: colors.slate[400],
-        red: colors.red[900],
-        orange: colors.orange[900],
-        yellow: colors.yellow[900],
-        green: colors.green[900],
-        blue: colors.blue[900],
-        purple: colors.purple[900],
-        pink: colors.pink[900],
-        teal: colors.teal[900],
-        cyan: colors.cyan[900],
-        rose: colors.rose[900],
-      },
-      fontFamily: {
-        'sans': ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+        background: "0F172A",
+        text: "F8FAFC",
+        surfaceDark: "1E293B",
+        surfaceLight: "64748B",
+        yellow: "FFD369"
       },
     },
     variants: {
       extend: {},
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-    ],
   }
 }
